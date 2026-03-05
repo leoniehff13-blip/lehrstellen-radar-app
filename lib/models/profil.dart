46ebb44a5f52f12ab275d9ebe54a4f5d1fa8ba91
leonie.hff13 <leonie.hff13@gmail.com>
2026-03-05 12:20:12 +0000

    feat: Profilbilder zur Talentliste hinzugefügt

--- lib/models/profil.dart
+++ lib/models/profil.dart
@@ -1,37 +1,21 @@
 class Profil {
   final String? name;
   final String? vorname;
-  final String? betrieb;
-  final String? strasse;
-  final String? hausnummer;
-  final String? plz;
-  final String? stadt;
-  final String? land;
-  final String? ansprechperson;
   final String? gewerk;
-  final String? profilTyp;
   final int? lehrjahr;
-  final String? unternehmen;
-  final String? handwerkskammer;
-  final String? spezialisierung;
   final List<String>? faehigkeiten;
+  final String? profilTyp; // 'Azubi' oder 'Betrieb'
+  final String? unternehmen; 
+  final String? profilbild; // URL zum Profilbild
 
   Profil({
     this.name,
     this.vorname,
-    this.betrieb,
-    this.strasse,
-    this.hausnummer,
-    this.plz,
-    this.stadt,
-    this.land,
-    this.ansprechperson,
     this.gewerk,
-    this.profilTyp,
     this.lehrjahr,
-    this.unternehmen,
-    this.handwerkskammer,
-    this.spezialisierung,
     this.faehigkeiten,
+    this.profilTyp,
+    this.unternehmen,
+    this.profilbild,
   });
 }
